ef43ab4d72607bd2bbb347ca4a2ef939ba8fa27f
murilocost <muriloalimacosta@hotmail.com>
2024-05-30 03:10:13 -0300

    Adição de lógica de inicialização de bombas no campo e introdução a lógica de varredura de bombas adjacentes aos campos que não contem bombas

--- campoMinado.c
+++ campoMinado.c
@@ -13,6 +13,9 @@ void inicializaCampo(char (*campo)[SIZE]) {
             else if (j == 0 && i > 0) {
                 campo[i][j] = '0' + i;
             }
+            else if (i == 0 && j == 0) {
+                campo[i][j] = '@';
+            }
             else {
                 campo[i][j] = '#';
             }
@@ -39,55 +42,107 @@ void inicializaMenu() {
 
 void printCampo(char campo[SIZE][SIZE]) {
     printf("\n");
+    printf("\n");
+
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            printf("%c ", campo[i][j]);
+            printf("%c  ", campo[i][j]);
         }
         printf("\n");
     }
+
+    printf("\n");
+}
+
+void inicializaBombas(int numBomb, char (*campo)[SIZE]) {
+    srand(time(NULL));
+
+    int cont = 0; // Contador de bombas colocadas
+
+    while (cont < numBomb) {
+        int colRand = (rand() % 9) + 1; // Gera uma coluna aleatória (1 a 9)
+        int linRand = (rand() % 9) + 1; // Gera uma linha aleatória (1 a 9)
+
+        // Verifica se a posição não tem uma bomba
+        if (campo[linRand][colRand] != 'B') {
+            campo[linRand][colRand] = 'B'; // Posiciona a bomba
+            cont++;
+        }
+    }
+
+    printCampo(campo);
+}
+
+void sensorBombas (char (*campo)[SIZE], char (*campoClone)[SIZE]) {  // NECESSITA DE CORREÇÃO NA VARREDURA DA MATRIX 3X3 PARA AS BOMBAS ADJACENTES
+    char bombas[9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+    int contaBomba = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (campo[i][j] != 'B' && i > 0 && j > 0) {
+                contaBomba = 0;
+
+                for (int k = -1; k <= 1; k++) {
+                    for (int q = -1; q <= 1; q++) {
+
+                        //if (i + k < SIZE && j + q < SIZE){
+                            if (campo[i + k][j + q] == 'B') contaBomba += 1;
+                        //}
+                        
+                    }
+                }
+
+                campoClone[i][j] = bombas[contaBomba];
+            }
+        }
+    }
+
+    printCampo(campoClone);
 }
 
 int main() {
     int aux = 1;
 
     while (aux) {
-        srand(time(NULL));
 
-        char campo[SIZE][SIZE];
+        char campo[SIZE][SIZE], campoBombas[SIZE][SIZE], campoContador[SIZE][SIZE];
         
-        inicializaCampo(campo);
-
         inicializaMenu();
 
         int op;
 
         printf("\n\t\tESCOLHA: ");
 
-        scanf("%d'", &op);
+        scanf("%d", &op);
+
+        inicializaCampo(campo);
+        inicializaCampo(campoBombas);
+        inicializaCampo(campoContador);
 
         printCampo(campo);
 
         switch (op) {
             case 1:
-                // 3 bombas;
+                inicializaBombas(10, campoBombas);
+                sensorBombas(campo, campoBombas);
                 break;
             case 2:
-                // 10 bombas;
+                inicializaBombas(20, campoBombas);
+                sensorBombas(campo, campoBombas);
                 break;
             case 3:
-                // 28 bombas;
+                inicializaBombas(45, campoBombas);
+                sensorBombas(campo, campoBombas);
                 break;
             case 4:
-                // 35 bombas;
+                inicializaBombas(80, campoBombas);
+                sensorBombas(campo, campoBombas);
                 break;
             default:
                 aux = 0;
                 break;
-
         }
     }
-    
-
 
     return 0;
 }
